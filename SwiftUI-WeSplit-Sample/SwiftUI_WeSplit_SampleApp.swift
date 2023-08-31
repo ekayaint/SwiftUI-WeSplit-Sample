1945ec95ef77fccf71a229b589dc4b4e9980419b
ekayaint <ekayaint@gmail.com>
2023-08-31 20:21:38 +0300

    Initial Commit

---
+++ SwiftUI-WeSplit-Sample/SwiftUI_WeSplit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeSplit_SampleApp.swift
+//  SwiftUI-WeSplit-Sample
+//
+//  Created by ekayaint on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeSplit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
